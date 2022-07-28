7ec3102590db87afa57471736dc9fe7d5fdcd7f9
kamileq14 <kolokin987@gmail.com>
2022-07-28 12:51:01 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,142 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+boolean ShouldContinue = true;
+
+// created by kamileq_14
+        
+        String a = ("•—");
+        String b = ("—•••");
+        String c = ("—•—•");
+        String d = ("—••");
+        String e = ("•");
+        String f = ("••—•");
+        String g = ("——•");
+        String h = ("••••");
+        String i = ("••");
+        String j = ("•———");
+        String k = ("—•—");
+        String l = ("•—••");
+        String m = ("——");
+        String n = ("—•");
+        String o = ("———");
+        String p = ("•——•");
+        String q = ("——•—");
+        String r = ("•—•");
+        String s = ("•••");
+        String t = ("—");
+        String u = ("••—");
+        String v = ("•••—");
+        String w = ("•——");
+        String x = ("—••—");
+        String y = ("—•——");
+        String z = ("——••");
+
+        Scanner scanner = new Scanner(System.in);
+        while(ShouldContinue) {
+            System.out.println(" ");
+            System.out.println("this is simple translator from english to morse");
+            System.out.println("this translate only one letter per line");
+            System.out.println("if you want to translate letter write it");
+            System.out.println("if you want to stop write [stop]");
+            System.out.println("============================================================");
+            String userinput = scanner.next();
+
+            if (userinput.equals("a")) {
+                System.out.println(a);
+            }
+            if (userinput.equals("b")) {
+                System.out.println(b);
+            }
+            if (userinput.equals("c")) {
+                System.out.println(c);
+            }
+            if (userinput.equals("d")) {
+                System.out.println(d);
+            }
+            if (userinput.equals("e")) {
+                System.out.println(e);
+            }
+            if (userinput.equals("f")) {
+                System.out.println(f);
+            }
+            if (userinput.equals("g")) {
+                System.out.println(g);
+            }
+            if (userinput.equals("h")) {
+                System.out.println(h);
+            }
+            if (userinput.equals("i")) {
+                System.out.println(i);
+            }
+            if (userinput.equals("j")) {
+                System.out.println(j);
+            }
+            if (userinput.equals("k")) {
+                System.out.println(k);
+            }
+            if (userinput.equals("l")) {
+                System.out.println(l);
+            }
+            if (userinput.equals("m")) {
+                System.out.println(m);
+            }
+            if (userinput.equals("n")) {
+                System.out.println(n);
+            }
+            if (userinput.equals("o")) {
+                System.out.println(o);
+            }
+            if (userinput.equals("p")) {
+                System.out.println(p);
+            }
+            if (userinput.equals("q")) {
+                System.out.println(q);
+            }
+            if (userinput.equals("r")) {
+                System.out.println(r);
+            }
+            if (userinput.equals("s")) {
+                System.out.println(s);
+            }
+            if (userinput.equals("t")) {
+                System.out.println(t);
+            }
+            if (userinput.equals("u")) {
+                System.out.println(u);
+            }
+            if (userinput.equals("v")) {
+                System.out.println(v);
+            }
+            if (userinput.equals("w")) {
+                System.out.println(w);
+            }
+            if (userinput.equals("x")) {
+                System.out.println(x);
+            }
+            if (userinput.equals("y")) {
+                System.out.println(y);
+            }
+            if (userinput.equals("z")) {
+                System.out.println(z);
+            }
+if (userinput.equals("stop")) {
+    break;
+}
+
+        }
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
